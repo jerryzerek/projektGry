92c4af5d8eee956a17039cba4e0c775748591ae6
Jedrzej Zerek <Jedrzej.Zerek@pl.ey.com>
2018-11-27 22:01:48 +0100

    javaFX

---
+++ src/main/java/com/kodilla/Blackjack.java
@@ -0,0 +1,33 @@
+package com.kodilla;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+public class Blackjack extends Application {
+
+    private Image imageback = new Image("file:resources/images/Tondenga.jpg");
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        BackgroundSize backgroundSize = new BackgroundSize(500, 500, true, true, true, false);
+        BackgroundImage backgroundImage = new BackgroundImage(imageback, BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, backgroundSize);
+        Background background = new Background(backgroundImage);
+
+        GridPane grid = new GridPane();
+        grid.setBackground(background);
+
+        Scene scene = new Scene(grid, 600, 300, Color.BLACK);
+
+        primaryStage.setTitle("BlackJack");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
